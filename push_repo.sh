d927fdad0ca7a7d86e17db95db6a1d6340bf5ae0
yuanhang <yuanhang1@100tal.com>
2019-08-29 19:16:48 +0800

    修改脚本

--- push_repo.sh
+++ push_repo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 pod repo update minlison
 echo "-------发布 $1 到 minlison 库...-------"
-pod repo push minlison MLSModel.podspec  --sources=https://github.com/Minlison/Specs.git,master
+pod repo push minlison MLSNetwork.podspec  --sources=https://github.com/Minlison/Specs.git,master --allow-warnings
 echo "-------发布 $1 到 minlison 库完成-------"
 pod repo update minlison
